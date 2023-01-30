1283be264070c6b76d058602c05eefd199353e8e
Brandon <branrtread@gmail.com>
2023-01-30 15:18:43 -0400

    Added validation for Team and Player numbers as well as Team name. Validation needed for player stats.

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,65 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        boolean inputCorrect = true;
+        int numTeams = 0;
+        int numPlayers = 0;
 
 
         //region System Start
         System.out.println("FANTASY HOCKEY");
 
-        System.out.print("Enter how many teams will be participating: ");
-        int numTeams = scan.nextInt();
+
+        do {
+            System.out.print("Enter how many teams will be participating: ");
+            while(!scan.hasNextInt()){
+                System.out.println();
+                scan.next();
+                System.out.print("Please enter a number: ");
+            }
+            numTeams = scan.nextInt();
+            if(numTeams < 2){
+                System.out.println("Please enter a number greater than 1");
+                System.out.println();
+            }
+        } while (numTeams < 2);
         System.out.println();
 
         Team[] teams = new Team[numTeams];
 
-        System.out.print("Enter how many players will be on each team: ");
-        int numPlayers = scan.nextInt();
+        do {
+            System.out.print("Enter how many players will be on each team: ");
+            while(!scan.hasNextInt()){
+                System.out.println();
+                scan.next();
+                System.out.print("Please enter a number: ");
+            }
+            numPlayers = scan.nextInt();
+            if(numPlayers < 1){
+                System.out.println("Please enter a number greater than 0.");
+                System.out.println();
+            }
+
+        } while (numPlayers < 1);
+
         Player[][] players = new Player[numTeams * numPlayers][numPlayers];
         System.out.println();
         String blank = scan.nextLine();
+
         //endregion
 
         //region Team Entry
         System.out.println("TEAM ENTRY");
         for (int i = 0; i < numTeams; i++) {
-            System.out.print("Enter name for team #" + (i + 1) + ": ");
-            teams[i] = new Team(scan.nextLine(), i);
-            System.out.println();
+            do {
+                System.out.print("Enter name for team #" + (i + 1) + ": ");
+                teams[i] = new Team(scan.nextLine(), i);
+
+                if(teams[i].getName().length() < 3){
+                    System.out.println("Please enter a name at least 3 characters long.");
+                }
+                System.out.println();
+            }while(teams[i].getName().length() < 3);
         }
         //endregion
 
